7d0d77c2b6dfa6b03d9ecfd14f394777a696bb94
Colby Fayock <fayock@gmail.com>
2023-08-19 22:58:30 -0400

    Step 2: Installing and configuring the Appwrite SDK

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,7 @@
+import { Client } from 'appwrite';
+
+export const client = new Client();
+
+client
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
